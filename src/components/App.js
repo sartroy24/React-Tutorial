38ed6267a02e8ed7ad3a4efd5b6a34983ea32952
sartroy24 <sarthakroy10@gmail.com>
2021-12-19 11:40:03 +0530

    1 binaries uploaded

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,23 @@ import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 export default function App() {
+  const contacts = [
+    {
+      id: 1,
+      "name": "Sarthak",
+      "email": "sarthak@gmail.com"
+    },
+    {
+      id: 2,
+      "name":"Roy",
+      "email": "roy@gmail.com"
+    }
+  ]
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import ContactCard from './ContactCard';
+const ContactList = (props) => {
+  console.log('Contacts -->' + props);
 
-const ContactList = () => {
-  return <div className="ui celled list">Contact List</div>;
+  const renderContactList = props.contacts.map((contact) => {
+    return <ContactCard contact={contact}></ContactCard>;
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
 };
 
 export default ContactList;
